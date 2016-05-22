e86925034feee1ed996b52b5f689b5e0b9435a40
digitalLumberjack <digitallumberjack@gmail.com>
2016-05-22 14:45:48 +0200

    added IP print for A record in example

--- example/example.go
+++ example/example.go
@@ -3,29 +3,34 @@ package main
 import (
 	"github.com/digitallumberjack/dnsapi"
 	"github.com/Sirupsen/logrus"
+	"github.com/miekg/dns"
 )
 
 func main() {
 
 	/** Creates a new dns api (c2VjcmV0 = secret in base64) **/
-	dns := dnsapi.NewDNSApi("10.10.10.10", "53", "secret", "c2VjcmV0", "mydomain.org")
+	dnsapi := dnsapi.NewDNSApi("10.10.10.10", "53", "secret", "c2VjcmV0", "mydomain.org")
 	{
-		list, err := dns.list()
+		list, err := dnsapi.List()
 		if (err != nil) {
 			logrus.Fatalf("error %v", err)
 		}
 		for _, e := range list {
+			switch e.(type){
+			case *dns.A:
+				logrus.Infof("%s", e.(*dns.A).A)
+			}
 			logrus.Infof("%s", e.String())
 		}
 	}
 	{
-		err := dns.add("www.mydomain.org", "1.1.1.1", "A", 60)
+		err := dnsapi.Add("www.mydomain.org", "1.1.1.1", "A", 60)
 		if (err != nil) {
 			logrus.Fatalf("error %v", err)
 		}
 	}
 	{
-		err := dns.remove("www.mydomain.org", "A")
+		err := dnsapi.Remove("www.mydomain.org", "A")
 		if (err != nil) {
 			logrus.Fatalf("error %v", err)
 		}
